94806b7c03c1f40f7c28a33b110c727c503c2b1c
praful <prafulkusugal@gmail.com>
2026-03-23 10:30:17 +0530

    root api

--- admin-be/src/app.js
+++ admin-be/src/app.js
@@ -9,6 +9,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
 
 app.use("/api/users", usersRoutes);
 app.use("/api/auth", authRoutes);
