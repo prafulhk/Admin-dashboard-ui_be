a983ea0cdb4d4d3ef5605069ea6eafc098bf62a4
praful <prafulkusugal@gmail.com>
2026-03-23 09:41:25 +0530

    added extension

--- admin-be/server.js
+++ admin-be/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-const app = require("./src/app");
+const app = require("./src/app.js");
 
-const connectDB = require("./src/config/db");
+const connectDB = require("./src/config/db.js");
 
 const PORT = process.env.PORT || 3000;
 
