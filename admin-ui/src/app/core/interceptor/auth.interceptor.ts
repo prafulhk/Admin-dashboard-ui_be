941d99c95e1c8816a21899ab0dff23e93fd4fb37
praful <prafulkusugal@gmail.com>
2026-03-24 05:50:26 +0530

    error handling and redirects on session expired

--- admin-ui/src/app/core/interceptor/auth.interceptor.ts
+++ admin-ui/src/app/core/interceptor/auth.interceptor.ts
@@ -1,17 +1,37 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { ToastService } from '../services/toast-service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  const toast = inject(ToastService);
+
   const token = localStorage.getItem('token');
 
+  let cloned = req;
+
   if (token) {
-    const cloned = req.clone({
+    cloned = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
       },
     });
-
-    return next(cloned);
   }
 
-  return next(req);
+  return next(cloned).pipe(
+    catchError((error) => {
+      // 🔴 Unauthorized
+      if (error.status === 401) {
+        localStorage.removeItem('token');
+        router.navigate(['/login']);
+        toast.showError('Session expired. Please login again');
+      } else {
+        toast.showError(error.error?.message || 'Something went wrong');
+      }
+
+      return throwError(() => error);
+    }),
+  );
 };
