74087d072b8d77d1e85a9d6607bd8980cdb7f933
praful <prafulkusugal@gmail.com>
2026-04-01 06:00:08 +0530

    updated role based access

--- admin-ui/src/app/core/services/auth-service.ts
+++ admin-ui/src/app/core/services/auth-service.ts
@@ -1,12 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environment';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  getUserFromToken(): any {
+    const token = this.getToken();
+    return token ? jwtDecode(token) : null;
+  }
+
+  // getRole(): string | null {
+  //   return this.getUserFromToken()?.role || null;
+  // }
+
+  getRole(): string | null {
+    let token = localStorage.getItem('token');
+    const decodedToken: any = token ? jwtDecode(token) : null;
+    return decodedToken?.role || null;
+  }
 
   login(data: any) {
     return this.http.post(`${environment.apiUrl}/auth/login`, data);
--- admin-ui/src/app/layout/sidebar/sidebar.ts
+++ admin-ui/src/app/layout/sidebar/sidebar.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { LayoutService } from '../../core/services/layout-service';
 
 import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth-service';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,15 +11,21 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './sidebar.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Sidebar {
+export class Sidebar implements OnInit {
   isOpen = true;
   @Input() isMobileOpen = false;
+  role!: string | null;
   constructor(
     private layout: LayoutService,
     public router: Router,
+    public authService: AuthService,
   ) {
     this.layout.sidebarState$.subscribe((state) => {
       this.isOpen = state;
     });
+  }
+
+  ngOnInit() {
+    this.role = this.authService.getRole();
   }
 }
