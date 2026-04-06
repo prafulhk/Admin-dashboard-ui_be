f7ddb1cd816446e0a81e5e5e20d1193402b9625a
praful <prafulkusugal@gmail.com>
2026-04-06 06:11:45 +0530

    implemented with async to improve performance

--- admin-ui/src/app/features/dashboard/dashboard.ts
+++ admin-ui/src/app/features/dashboard/dashboard.ts
@@ -1,153 +1,51 @@
-import { Component, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Card } from '../../shared/components/card/card';
-import { Table } from '../../shared/components/table/table';
-import { Modal } from '../../shared/components/modal/modal';
 import { FormsModule } from '@angular/forms';
 import { Charts } from '../../shared/components/charts/charts';
-import { ToastService } from '../../core/services/toast-service';
-import { Toast } from '../../shared/components/toast/toast/toast';
 import { Skeleton } from '../../shared/components/skeleton/skeleton';
-
-import { User, UserService } from '../../core/services/user-service';
+import { User } from '../../core/services/user-service';
 import { Store } from '@ngrx/store';
-import { addUser, loadUsers, deleteUser, updateUser } from '../../store/users/users.actions';
+import { loadUsers } from '../../store/users/users.actions';
 import { selectAllUsers } from '../../store/users/users.selectors';
-import { selectUserRole } from '../../store/auth/auth.selectors';
+import { map, Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Card, Table, Modal, FormsModule, Charts, Toast, Skeleton, AsyncPipe],
+  imports: [Card, FormsModule, Charts, Skeleton, AsyncPipe],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Dashboard {
-  // isModalOpen = false;
-  // isEditMode = false;
-  // editIndex = -1;
-  // deleteIndex = -1;
-  // deleteId: string = '';
-  // isDeleteModalOpen = false;
   pieChartLabel = ['Admin', 'Editor', 'User'];
   lineChartLabel = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
   toastTimer: any;
-  // columns = ['Name', 'Email', 'Role', 'Status'];
-  // searchText = '';
   isLoading = false;
-  // filteredUsers: User[] = [];
   roleChartData: number[] = [0, 0, 0];
-  // newUser = {
-  //   name: '',
-  //   email: '',
-  //   role: 'User',
-  //   status: 'Active',
-  //   _id: '',
-  // };
-  users: User[] = [];
-  // role$: any;
-  adminCount = 0;
-  activeUsersCount = 0;
-  totalUsers = 0;
+  users$!: Observable<User[]>;
+  adminCount$: Observable<number> = new Observable();
+  activeUsersCount$: Observable<number> = new Observable();
 
-  constructor(
-    private toast: ToastService,
-    private cdr: ChangeDetectorRef,
-    private store: Store,
-    private userService: UserService,
-  ) {}
+  constructor(private store: Store) {}
 
   ngOnInit() {
     this.isLoading = true;
-    // this.role$ = this.store.select(selectUserRole);
-    // this.role$.subscribe((role: any) => console.log('User Role:', role));
-
     this.store.dispatch(loadUsers());
-
     this.store.select(selectAllUsers).subscribe((users) => {
-      this.users = users;
-      // this.filteredUsers = users;
-      this.totalUsers = users.length;
-      this.adminCount = users.filter((u) => u.role.toLowerCase() === 'Admin'.toLowerCase()).length;
-      this.activeUsersCount = users.filter(
-        (u) => u.status.toLowerCase() === 'Active'.toLowerCase(),
-      ).length;
-
+      this.users$ = this.store.select(selectAllUsers);
+      this.adminCount$ = this.users$.pipe(
+        map((users) => users.filter((u) => u.role.toLowerCase() === 'Admin'.toLowerCase()).length),
+      );
+      this.activeUsersCount$ = this.users$.pipe(
+        map(
+          (users) => users.filter((u) => u.status.toLowerCase() === 'Active'.toLowerCase()).length,
+        ),
+      );
       this.roleChartData = this.calculateRoleStats(users);
       this.isLoading = false;
-      this.cdr.detectChanges();
     });
   }
-
-  // openModal() {
-  //   this.isEditMode = false;
-
-  //   this.newUser = {
-  //     name: '',
-  //     email: '',
-  //     role: 'User',
-  //     status: 'Active',
-  //     _id: '',
-  //   };
-
-  //   this.isModalOpen = true;
-  // }
-
-  // closeModal() {
-  //   this.isModalOpen = false;
-  // }
-
-  // editUser(user: any, index: number) {
-  //   this.newUser = { ...user };
-
-  //   this.isEditMode = true;
-  //   this.editIndex = index;
-
-  //   this.isModalOpen = true;
-  // }
-
-  // saveUser() {
-  //   if (this.isEditMode) {
-  //     this.userService.updateUser(this.newUser._id, this.newUser).subscribe((updatedUser) => {
-  //       if (updatedUser) {
-  //         this.store.dispatch(
-  //           updateUser({
-  //             index: this.editIndex,
-  //             user: updatedUser,
-  //           }),
-  //         );
-  //         this.toast.show('User Updated');
-  //       } else {
-  //         this.toast.show('Failed to update user');
-  //       }
-  //     });
-  //   } else {
-  //     this.userService.addUser(this.newUser).subscribe((createdUser) => {
-  //       if (createdUser) {
-  //         this.store.dispatch(addUser({ user: createdUser }));
-  //         this.toast.show('User Added');
-  //       } else {
-  //         this.toast.show('Failed to add user');
-  //       }
-  //     });
-  //   }
-
-  //   this.closeModal();
-  // }
-
-  // confirmDelete(event: any) {
-  //   this.deleteIndex = event.index;
-  //   this.deleteId = event.row._id;
-  //   this.isDeleteModalOpen = true;
-  // }
-
-  // deleteUser() {
-  //   this.userService.deleteUser(this.deleteId).subscribe(() => {
-  //     this.store.dispatch(deleteUser({ index: this.deleteIndex }));
-  //     this.isDeleteModalOpen = false;
-  //     this.toast.show('User Deleted');
-  //   });
-  // }
 
   calculateRoleStats(users: User[]) {
     const roles = { Admin: 0, Editor: 0, User: 0 };
@@ -158,28 +56,4 @@ export class Dashboard {
     });
     return [roles.Admin, roles.Editor, roles.User];
   }
-
-  getTotalUsers() {
-    return this.users.length;
-  }
-
-  getAdminCount() {
-    return this.users.filter((user) => user.role === 'Admin').length;
-  }
-
-  getActiveUsers() {
-    return this.users.filter((user) => user.status === 'Active').length;
-  }
-
-  // onSearch() {
-  //   const search = this.searchText.toLowerCase();
-
-  //   this.filteredUsers = this.users.filter(
-  //     (user) =>
-  //       user.name.toLowerCase().includes(search) ||
-  //       user.email.toLowerCase().includes(search) ||
-  //       user.role.toLowerCase().includes(search) ||
-  //       user.status.toLowerCase().includes(search),
-  //   );
-  // }
 }
